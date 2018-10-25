6756e847801730f6ce04d4b1a1c71cb7bbc594b1
archistico <info@archistico.com>
2018-10-25 09:55:05 +0200

    presentazione domande vue

--- app.js
+++ app.js
@@ -2,14 +2,46 @@ import { Domanda, Risposta, Effetto, Simulatore, log } from "./external.js";
 
 let sim = new Simulatore();
 
-sim.domande.push(new Domanda("Domanda", 
-                new Risposta("Sì", new Effetto(10,0,0)), 
-                new Risposta("No", new Effetto(0,10,0))
-                ));
+sim.domande.push(new Domanda("Vuoi fare guerra il primo giorno?",
+    new Risposta("Sì", new Effetto(10, 0, 0)),
+    new Risposta("No", new Effetto(-10, 0, 0))
+));
+
+sim.domande.push(new Domanda("Bisogna aumentare le tasse?",
+    new Risposta("Sì", new Effetto(0, 5, 0)),
+    new Risposta("No", new Effetto(0, -5, 0))
+));
+
+var app = new Vue({
+    el: '#app',
+    data: {
+        domanda: sim.getDomanda(),
+        risposta1: sim.getRisposta1(),
+        risposta2: sim.getRisposta2(),
+    },
+    methods: {
+        risposta1_click: function () {
+            sim.risposto(0)
+            sim.avanza()
+            domanda: sim.getDomanda()
+            risposta1: sim.getRisposta1()
+            risposta2: sim.getRisposta2()
+        },
+        risposta2_click: function () {
+            sim.risposto(1)
+            sim.avanza()
+            domanda: sim.getDomanda()
+            risposta1: sim.getRisposta1()
+            risposta2: sim.getRisposta2()
+        }
+    }
+})
 
 
+/*
 log("---START---");
-sim.setRisposte([0]);
+sim.setRisposte([0, 1]);
 sim.simula();
 log(sim);
 log("---STOP----");
+*/
--- external.js
+++ external.js
@@ -1,7 +1,7 @@
 export const log = message => console.log(message);
 
 export class Effetto {
-    constructor (denaro, salute, giustizia) {
+    constructor(denaro, salute, giustizia) {
         this.denaro = denaro;
         this.salute = salute;
         this.giustizia = giustizia;
@@ -9,14 +9,14 @@ export class Effetto {
 }
 
 export class Risposta {
-    constructor (testo, effetto) {
+    constructor(testo, effetto) {
         this.testo = testo;
         this.effetto = effetto;
     }
 }
 
 export class Domanda {
-    constructor (testo, risposta1, risposta2) {
+    constructor(testo, risposta1, risposta2) {
         this.testo = testo;
         this.risposta1 = risposta1;
         this.risposta2 = risposta2;
@@ -30,10 +30,11 @@ export class Simulatore {
         this.salute = 100;
         this.giustizia = 100;
         this.risposte = [];
+        this.contatore = 0;
     }
 
     getParametri() {
-        log("Denaro: "+this.denaro+" | Salute: "+this.salute+" | Giustizia: "+this.giustizia);
+        log("Denaro: " + this.denaro + " | Salute: " + this.salute + " | Giustizia: " + this.giustizia);
         log("-----------------------------");
     }
 
@@ -47,19 +48,44 @@ export class Simulatore {
         this.getParametri();
         let contRisp = 0;
         this.domande.forEach(dom => {
-            log(dom.testo+"? : "+ dom.risposta1.testo + " / "+dom.risposta2.testo);
-            if(this.risposte[contRisp] == 0) {
+            log(dom.testo + "? : " + dom.risposta1.testo + " / " + dom.risposta2.testo);
+            if (this.risposte[contRisp] == 0) {
                 this.aggiorna(dom.risposta1.effetto);
             } else {
                 this.aggiorna(dom.risposta2.effetto);
             }
             this.getParametri();
-            contRisp+=1;
+            contRisp += 1;
         });
     }
 
+    risposto(ris) {
+        if (ris == 0) {
+            this.aggiorna(this.domande[this.contatore].risposta1.effetto);
+        } else {
+            this.aggiorna(this.domande[this.contatore].risposta2.effetto);
+        }
+        this.getParametri();
+    }
+
+    avanza() {
+        this.contatore+=1;
+    }
+
+    getDomanda() {
+        return this.domande[this.contatore].testo;
+    }
+
+    getRisposta1() {
+        return this.domande[this.contatore].risposta1.testo;
+    }
+
+    getRisposta2() {
+        return this.domande[this.contatore].risposta2.testo;
+    }
+
     setRisposte(risposte) {
-        if(typeof(risposte) !== "undefined" && risposte !== null && this.domande.length == risposte.length) {
+        if (typeof (risposte) !== "undefined" && risposte !== null && this.domande.length == risposte.length) {
             this.risposte = risposte;
         } else {
             throw new Error("Errore il numero delle risposte non copre tutte le domande");
